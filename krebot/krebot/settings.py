daaa0b96485279009d77f84d90368c004db73a7b
12ilya21 <121348120+12ilya21@users.noreply.github.com>
2024-06-04 00:41:33 +0900

    Update settings.py

--- krebot/krebot/settings.py
+++ krebot/krebot/settings.py
@@ -132,13 +132,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# 정적 파일들이 모일 최종 경로
+# 정적 파일들이 이미 존재하는 디렉토리를 지정
 STATIC_ROOT = '/home/ubuntu/venvs/env/lib/python3.10/site-packages/rest_framework/static/'
 
-# 추가적인 정적 파일 디렉토리
-STATICFILES_DIRS = [
-    '/home/ubuntu/venvs/env/lib/python3.10/site-packages/rest_framework/static/rest_framework/'
-]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
